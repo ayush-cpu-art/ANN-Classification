936daf5cdb130b0ac31560482cbf63eecc2a49d8
ayush-cpu-art <ayushdev0408@gmail.com>
2026-06-15 23:56:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pickle
 
 # load the trained model
-model=tf.keras.models.load_model('my_model.keras')
+model = tf.keras.models.load_model('my_model.keras',compile=False)
 
 # Load the enncoder and scaler
 with open('onehot_encoder_geo.pkl','rb') as file:
